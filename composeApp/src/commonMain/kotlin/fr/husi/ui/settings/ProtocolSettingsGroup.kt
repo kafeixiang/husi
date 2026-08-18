2bc3b1510620aaa25a287c24360a09b38b0f4aa2
kafeixiang <kafeixiang@example.com>
2026-08-18 22:42:02 +0800

    mieruProvider

--- composeApp/src/commonMain/kotlin/fr/husi/ui/settings/ProtocolSettingsGroup.kt
+++ composeApp/src/commonMain/kotlin/fr/husi/ui/settings/ProtocolSettingsGroup.kt
@@ -20,6 +20,7 @@ import fr.husi.resources.hysteria2_provider
 import fr.husi.resources.hysteria_download_mbps
 import fr.husi.resources.hysteria_upload_mbps
 import fr.husi.resources.juicity_provider
+import fr.husi.resources.mieru_provider
 import fr.husi.resources.plugin
 import fr.husi.resources.provider_naive
 import fr.husi.ui.StringOrRes
@@ -115,6 +116,29 @@ internal fun ProtocolSettingsGroup(
         type = ListPreferenceType.DROPDOWN_MENU,
         valueToText = { AnnotatedString(stringOrRes(pluginProviderText(it))) },
     )
+
+    val mieruProviderValue by DataStore.configurationStore
+        .intFlow(Key.PROVIDER_MIERU, ProtocolProvider.PLUGIN)
+        .collectAsStateWithLifecycle(ProtocolProvider.PLUGIN)
+    ListPreference(
+        value = mieruProviderValue,
+        onValueChange = {
+            DataStore.providerMieru = it
+            needReload()
+        },
+        values = listOf(ProtocolProvider.CORE, ProtocolProvider.PLUGIN),
+        title = { Text(stringResource(Res.string.mieru_provider)) },
+        icon = {
+            MaskedIcon(
+                Res.drawable.flight_takeoff,
+                color = IconMaskColors.IconLightYellow,
+            )
+        },
+        summary = { Text(stringOrRes(pluginProviderText(mieruProviderValue))) },
+        type = ListPreferenceType.DROPDOWN_MENU,
+        valueToText = { AnnotatedString(stringOrRes(pluginProviderText(it))) },
+    )
+    PreferenceDivider()
 
     val naiveProviderValue by DataStore.providerNaive.collectAsStateWithLifecycle()
     ListPreference(
